0405740fb5e970ab09482e91f32290fdcb89cb1d
Gordon325 <gsmith50@eagles.ewu.edu>
2017-10-25 00:02:55 -0700

    Moving Title

--- js/scripts.js
+++ js/scripts.js
@@ -21,3 +21,21 @@ $(document).ready(function() {
 		$('.clickme').toggle();
 	});
 });
+
+
+
+   var baloon = $('#title .char1');
+   var baloon2 = $('#title .char2');
+var baloon3 = $('#title .char3');
+   function runIt() {
+       baloon.animate({top:'+=10'}, 2200);
+       baloon.animate({top:'-=10'}, 2200, runIt);
+       baloon2.animate({top:'-=5'}, 1600);
+       baloon2.animate({top:'+=5'}, 1600, runIt);
+       baloon3.animate({top:'-=15'}, 1900);
+       baloon3.animate({top:'+=15'}, 1900, runIt);
+   }
+
+
+   runIt();
+
